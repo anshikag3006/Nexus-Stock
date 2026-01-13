83eb429eab29ca1a9cfa553e3134f18bb3f5ab42
Anshika <anshika.22b0151028@abes.ac.in>
2026-01-14 01:28:44 +0530

    Security: Moved database credentials to environment variables

--- database.py
+++ database.py
@@ -1,6 +1,11 @@
+import os
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-db_url = "postgresql://postgres:Pass%402650@localhost:5432/anshika"
+load_dotenv()
+
+db_url = os.getenv("DATABASE_URL")
 engine = create_engine(db_url)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)

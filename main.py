4fb1159d40b7aa5fd71a206436b5593c7849e52a
Anshika <anshika.22b0151028@abes.ac.in>
2026-06-05 11:46:01 +0530

    add Vercel URL to CORS origins

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins = ["http://localhost:3000",
-                     "http://127.0.0.1:3000"],
+                     "http://127.0.0.1:3000",
+                     "https://nexus-stock-ebon.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
